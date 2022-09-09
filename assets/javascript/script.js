942d2cf891d129ae3a65b1b6ef0ebabcad1bbc0b
Caleb775 <89751339+Caleb775@users.noreply.github.com>
2022-09-08 21:27:31 -0700

    new commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,3 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Hello Bulma!');
   });
   
+
+  const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd1aa63a368msh58eb078b151ed8fp1005f6jsn83a1f7b7371f',
+		'X-RapidAPI-Host': 'theaudiodb.p.rapidapi.com'
+	}
+};
+
+fetch('https://theaudiodb.p.rapidapi.com/searchalbum.php?s=daft_punk', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
